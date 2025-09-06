149a7c49d8da538ffc10d53ce744b9ccb73d8334
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-09-06 13:45:37 +0300

    tested start

--- workshop_service/app/main.py
+++ workshop_service/app/main.py
@@ -1,18 +1,10 @@
-import asyncio
 from fastapi import FastAPI
-
-from config import settings
-from db.base import Base
-from db.session import engine
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title='WorkShop Service', docs_url='/')
+app = FastAPI(title="Test FastAPI App", version="1.0.0")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")
+async def root():
+    return {"message": "Hello World!", "status": "OK"}
+
 
