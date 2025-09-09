3b4230647d365d40ab46599da12d80c3b864de40
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-09-09 10:59:27 +0300

    fixed extra fields

--- user_profile/app/config.py
+++ user_profile/app/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     def RABBIT_URL(self):
         return f"{self.RABBITMQ_URL}"
         
-    model_config = SettingsConfigDict(env_file='.env',env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='.env',env_file_encoding='utf-8',extra='ignore')
 
 
 settings = Settings()
