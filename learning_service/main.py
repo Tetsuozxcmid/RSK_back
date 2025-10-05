096a3955a669f1fd8c769910a8750fb03648d27b
kirill <kirisa49@gmail.com>
2025-10-06 01:11:10 +0700

    fix

--- learning_service/main.py
+++ learning_service/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.routes.coures_routes.route import router as courses_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Learning Service", version="1.0.0")
+
+app = FastAPI(title="Learning Service", version="1.0.0", root_path="/learning")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(courses_router)
