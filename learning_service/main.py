87ffddf765c91d70565b6da1d03b2d413e06f6aa
kirill <kirisa49@gmail.com>
2025-10-06 00:57:56 +0700

    fix

--- learning_service/main.py
+++ learning_service/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from app.routes.videos import router as videos_router
-from app.routes.courses import router as courses_router
+from app.routes.coures_routes.route import router as courses_router
 
 app = FastAPI(title="Learning Service", version="1.0.0")
 
-app.include_router(videos_router)
 app.include_router(courses_router)
 
 
