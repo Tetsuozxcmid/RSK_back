5373a6e896d0e55381229d98472609b58ea1dcc5
kirill <kirisa49@gmail.com>
2025-10-06 02:37:24 +0700

    added progress

--- learning_service/app/db/models/course.py
+++ learning_service/app/db/models/course.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String
 from app.db.base import Base
 
 class Course(Base):
@@ -11,4 +11,3 @@ class Course(Base):
     description: Mapped[int] = mapped_column(String(255), nullable=True)
     file_extension: Mapped[str] = mapped_column(String(10), nullable=False)
     download_url: Mapped[str] = mapped_column(String(500), nullable=False)
-    is_completed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
