16e3b3518e2b1c767318c5405d01237bc135531d
kirill <kirisa49@gmail.com>
2025-10-06 02:39:58 +0700

    fix

--- learning_service/app/db/models/course.py
+++ learning_service/app/db/models/course.py
@@ -8,6 +8,6 @@ class Course(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     lesson_name: Mapped[str] = mapped_column(String(200), nullable=False)
     lesson_number: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[int] = mapped_column(String(255), nullable=True)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
     file_extension: Mapped[str] = mapped_column(String(10), nullable=False)
     download_url: Mapped[str] = mapped_column(String(500), nullable=False)
