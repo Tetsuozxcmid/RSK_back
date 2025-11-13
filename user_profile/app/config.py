be0d14dbd3726adf0839297c5a90a4e4e1098370
Tetsuozxcmid <tetsuonakamura6@gmail.com>
2025-11-13 16:08:35 +0300

    beta 0.2/fixed config

--- user_profile/app/config.py
+++ user_profile/app/config.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     DB_PASS: int
     DB_NAME: str
 
-    #RABBITMQ_URL: str
-    #SECRET_KEY: str
-    #ALGORITHM: str
+    RABBITMQ_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
     
 
 
@@ -17,13 +17,13 @@ class Settings(BaseSettings):
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
-    #def RABBIT_URL(self):
-        #return f"{self.RABBITMQ_URL}"
+    def RABBIT_URL(self):
+        return f"{self.RABBITMQ_URL}"
         
     model_config = SettingsConfigDict(env_file='.env',env_file_encoding='utf-8',extra='ignore')
 
 
 settings = Settings()
-#def get_auth_data():
-    #return {"secret_key" : settings.SECRET_KEY,"algorithm" : settings.ALGORITHM}
+def get_auth_data():
+    return {"secret_key" : settings.SECRET_KEY,"algorithm" : settings.ALGORITHM}
 
